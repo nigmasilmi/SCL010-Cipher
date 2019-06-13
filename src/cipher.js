47931ecc38c245dd3e22c6c26bccac21895c1293
nigm4 <nigmasilmi.web@gmail.com>
2019-06-12 20:53:01 -0400

    esqueleto básico y función cipher: done

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,33 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (offset, string) => {
+    const textoACifrar = string;
+    const toAddNumberInput = offset ;
+    //verificar si es viable usar let
+    var toAddNumber = null;
+    /* Vamos a generar un número entre 1 y 25 ya que el rango A-Z son 26 números (ascii 065-090)
+    tomando como argumento el valor del offset que ingrese el usuario
+    si el valor que ingresa el usuario está entre 1 y 25 */
+    if(toAddNumberInput >=1 && toAddNumberInput <=25){
+        toAddNumber = toAddNumberInput;
+    }else{
+        toAddNumber = Math.floor(toAddNumberInput / 25 + toAddNumberInput % 25);
+    }
+    if(toAddNumber >= 25){
+       do{
+        var toAddNumber2 = Math.floor(toAddNumber / 25 + offset % 25);   
+       }while ( (!toAddNumber2 >=1) && (!toAddNumber2 <=25));
+       //cuando salga del while, es decir que ya nuestra condición se cumpla, la asignamos a la variable toAddNumber
+        toAddNumber = toAddNumber2;
+    }
+  
+    //recorremos el texto y a cada caracter,
+    for(let i=0; i<textoACifrar.length; i++){
+        //ciframos el código ascii, lo parseamos a String y lo concatenamos con la cadena textoCifrado
+        textoCifrado += String.fromCharCode(textoACifrar.charCodeAt([i])+toAddNumber);
+        
+    }   
+    return(textoCifrado);   
+   }
     
   },
   decode: () => {
